47f3c696544c7ddcb2af6a4066d4be44d14fc643
Costas Kakousis <costas.kakousis@poltrex.com>
2019-02-25 14:54:32 +0200

    Added https property to mosca to allow wss

--- src/broker.js
+++ src/broker.js
@@ -7,6 +7,11 @@ var settings = {
     port: 8080,
     bundle: true,
     static: "./"
+  },
+  https: {
+    port: 443,
+    bundle: true,
+    static: "./"
   }
 };
 
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     uri: process.env.REACT_APP_PROD
-      ? "wss://mqtt-thermometer.herokuapp.com:8080"
+      ? "wss://mqtt-thermometer.herokuapp.com:443"
       : "ws://localhost:8080",
 
   }
