84d5c6d09fbf68f9d9a492314e398ba13b274005
Costas Kakousis <costas.kakousis@poltrex.com>
2019-02-26 17:09:28 +0200

    Heroku specific configs

--- src/config.js
+++ src/config.js
@@ -1,29 +1,42 @@
 module.exports = {
-    DEV: {
-      mqtt: {
-        host:"ws://localhost",
-        pub_topic: "temperature-topic",
-        sub_topic: "temperature-topic",
-        message: "",
-        polling_interval: 5000,
-        port:2221
-      },
-      ws: {
-        host: "ws://localhost",
-        port:2222
-      }
+  DEV: {
+    mqtt: {
+      host: "ws://localhost",
+      pub_topic: "temperature-topic",
+      sub_topic: "temperature-topic",
+      message: "",
+      polling_interval: 5000,
+      port: 2221
     },
-    PROD: {
-      mqtt: {
-        host: "mqtt://clouca.ddns.net",
-        pub_topic: "subDCA140A4AE30",
-        sub_topic: "pubDCA140A4AE30",
-        message: "req;iostatus",
-        polling_interval: 120000,
-      },
-      ws:{
-        host: "ws://localhost",
-        port: 2222
-      } 
+    ws: {
+      host: "ws://localhost",
+      port: 2222
     }
-}
+  },
+  PROD: {
+    mqtt: {
+      host: "mqtt://clouca.ddns.net",
+      pub_topic: "subDCA140A4AE30",
+      sub_topic: "pubDCA140A4AE30",
+      message: "req;iostatus",
+      polling_interval: 120000
+    },
+    ws: {
+      host: "ws://localhost",
+      port: 2222
+    }
+  },
+  HERO: {
+    mqtt: {
+      host: "mqtt://clouca.ddns.net",
+      pub_topic: "subDCA140A4AE30",
+      sub_topic: "pubDCA140A4AE30",
+      message: "req;iostatus",
+      polling_interval: 120000
+    },
+    ws: {
+      host: "ws://mqtt-thermometer.herokuapp.com",
+      port: 8080
+    }
+  }
+};
